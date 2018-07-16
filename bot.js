2afa929a6fa7d504ce5f06622e6302941e196411
Mohmaed Ayad <40663543+MohmaedAlhassny@users.noreply.github.com>
2018-07-16 23:09:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,35 +5,16 @@ const fs = require('fs')
 
 
 const moment = require("moment");
-client.on('message', async message => {
-    if (!message.channel.guild) return undefined;
-    let time = moment().format('Do MMMM YYYY , hh:mm');
-    let args = message.content.split(" ").slice(1).join(" ");
-    if(message.content.startsWith(prefix + "bc")) {
-        if(!message.guild.member(message.author).hasPermission("MANAGE_SERVER")) return message.reply("**# You don't have the needed permissions!**");
-        if(!args) return message.reply("**# Supply a message!**");
-        message.channel.send(`\`\`- Name:\`\`\n${message.author}\n\n\`\`- Date:\`\`\n${time}\n\n\`\`- Message:\`\`\n${args}\n\n__# | You have 15s to say يب or لا__`)
-.then(() => {
-  message.channel.awaitMessages(response => response.content === 'يب', {
-    max: 1,
-    time: 15000,
-    errors: ['time'],
-  })
-  .then((collected) => {
-          message.guild.members.forEach(m => m.sendMessage(args));
-          message.channel.send(`**Done!, Sent the message to: \`${message.guild.members.size}\` members!**`);
-
-  });
-});
-    } else {
-          message.channel.awaitMessages(response => response.content === 'لا', {
-    max: 1,
-    time: 15000,
-    errors: ['time'],
-  })
-  .then((collected) => {
-      message.channel.send("__- Canceled!__")
-    });
+client.on('message', msg =>{
+    let message=msg;
+    if(message.content.startsWith(prefix + "bc")){
+        var args = message.content.split(' ').slice(1).join(' ');
+    msg.guild.members.forEach(m=>{
+        m.send(args.replace(/[user]/g,m)).catch();
+    if(message.attachments.first()){
+m.sendFile(message.attachments.first().url).catch();
+    }
+    })    ;
     }
 });
 
